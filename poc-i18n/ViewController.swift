a1fe5462b0e6bfa6cbe2cff70c48b9abe42fbfb5
Thibaud B <thibaudb.com@gmail.com>
2017-10-26 08:15:51 +0200

    Initial Commit

---
+++ poc-i18n/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poc-i18n
+//
+//  Created by Thibaud on 26/10/2017.
+//  Copyright © 2017 tbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
